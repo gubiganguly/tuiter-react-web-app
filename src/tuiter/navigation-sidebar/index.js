570825e6b11a419cccd4cf063c90d2e30d6b4b84
gubiganguly <ganguly.n@northeastern.edu>
2022-10-23 18:14:18 -0400

    final

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -8,38 +8,38 @@ const NavigationSidebar = (
 ) => {
  return (
    <div className="list-group">
-     <a className="list-group-item"><i class="fa-brands fa-twitter"></i></a>
+     <a className="list-group-item"><i className="fa-brands fa-twitter"></i></a>
      <a className={`list-group-item
                     ${active === 'home'?'active':''}`}>
-       <i class="fa-sharp fa-solid fa-house"></i> Home
+       <i className="fa-sharp fa-solid fa-house"></i> Home
      </a>
      <a className={`list-group-item
                     ${active === 'explore'?'active':''}`}>
-       <i class="fa-solid fa-hashtag"></i> Explore
+       <i className="fa-solid fa-hashtag"></i> Explore
      </a>
      <a className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
-       <i class="fa-solid fa-bell"></i> Notifications
+       <i className="fa-solid fa-bell"></i> Notifications
      </a>
      <a className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
-       <i class="fa-sharp fa-solid fa-envelope"></i> Messages
+       <i className="fa-sharp fa-solid fa-envelope"></i> Messages
      </a>
      <a className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
-       <i class="fa-sharp fa-solid fa-bookmark"></i> Bookmarks
+       <i className="fa-sharp fa-solid fa-bookmark"></i> Bookmarks
      </a>
      <a className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
-       <i class="fa-solid fa-list"></i> Lists
+       <i className="fa-solid fa-list"></i> Lists
      </a>
      <a className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
-       <i class="fa-solid fa-user"></i> Profile
+       <i className="fa-solid fa-user"></i> Profile
      </a>
      <a className={`list-group-item
                     ${active === 'more'?'active':''}`}>
-      <i class="fa-solid fa-circle"></i> More
+      <i className="fa-solid fa-circle"></i> More
      </a>
    </div>
  );
